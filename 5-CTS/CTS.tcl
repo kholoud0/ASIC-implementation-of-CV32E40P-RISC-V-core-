e842a7365aefd530803752776d4f798fd1bef6c4
kholoud0 <khloud1khaled@gmail.com>
2024-03-07 03:45:59 +0200

    CTS_passed

--- 5-CTS/CTS.tcl
+++ 5-CTS/CTS.tcl
@@ -10,8 +10,8 @@ link_block
 ####################################################################################
 ###################################  CTS  ###################################
 ####################################################################################
-set_lib_cell_purpose -include cts \
-{HVT_lib/buf1 HVT_lib/buf2 LVT_lib/buf1 LVT_lib/buf2}
+
+set_dont_touch_network -clear [get_clocks CLK_I]
 
 ############################################################
 #source /home/islam/ICpedia_Tasks/GPCore-aes_ip/core_updated/syn/cons/dont_use_generic.tcl
@@ -19,13 +19,13 @@ set_lib_cell_purpose -include cts \
 #create_routing_rule ROUTE_RULES_1 \
  # -widths {M3 0.2 M4 0.2 } \
   #-spacings {M3 0.42 M4 0.63 }
-
-#set_clock_routing_rules -rules ROUTE_RULES_1 -min_routing_layer M2 -max_routing_layer M4
-
+#########################  include buffers to cts  ########################
+set_lib_cell_purpose -exclude cts [get_lib_cells -of [get_cells *]]
 set_lib_cell_purpose -include cts */*AOBUF_IW*
+set_lib_cell_purpose -include cts */*BUF*
 check_design -checks pre_clock_tree_stage
 
-create_routing_rule ROUTE_RULES_1 -multiplier_spacing 2 -multiplier_width 2
+create_routing_rule ROUTE_RULES_1 -multiplier_spacing 3 -multiplier_width 3
 set_clock_routing_rules -rules ROUTE_RULES_1 -min_routing_layer metal2  -max_routing_layer metal5
 set_clock_tree_options -target_latency 0.000 -target_skew 0.000 
 set cts_enable_drc_fixing_on_data true
@@ -34,11 +34,7 @@ clock_opt
 
 # clock_opt -from final_opto               #optimization
 
-write_verilog /home/ICer/cv32e40p_updated/results/${DESIGN}.cts.gate.v
-
-#report_qor > ../results/${DESIGN}.clock_qor.rpt
-
-#report_clock_timing  -type skew > ../results/${DESIGN}.clock_skew.rpt
+write_verilog /mnt/hgfs/Gp_CV32e40p/ASIC-Implementauion-of-CV32E40S-RISC-V-core-/results/${DESIGN}.cts.gate.v
 
 set_propagated_clock [get_clocks CLK_GATED]
 
@@ -53,3 +49,4 @@ report_qor > $dir/cts/qor.rpt
 create_utilization_configuration config_sr \
             -capacity site_row -exclude {hard_macros macro_keepouts}
 report_utilization -config config_sr -verbose > $dir/cts/utilization.rpt
+report_clock_timing  -type skew >  $dir/cts/clock_skew.rpt
