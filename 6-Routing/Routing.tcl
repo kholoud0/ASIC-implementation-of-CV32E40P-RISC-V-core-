677dc5a6e948a27b60b58caa601878446e9ee446
kholoud0 <khloud1khaled@gmail.com>
2024-03-10 01:39:28 +0200

    Update Routing.tcl

--- 6-Routing/Routing.tcl
+++ 6-Routing/Routing.tcl
@@ -8,14 +8,41 @@ set dir "/mnt/hgfs/Gp_CV32e40p/ASIC-Implementauion-of-CV32E40S-RISC-V-core-/repo
 ####################################################################################
 ###################################  ROUTING  ###################################
 ####################################################################################
+open_block /mnt/hgfs/Gp_CV32e40p/ASIC-Implementauion-of-CV32E40S-RISC-V-core-/2-Floorplan/scripts/riscv_core2:riscv_core_5_cts.design
+ link_block
 
 
 ############################################################
 remove_ignored_layers -all
+
+
+set MIN_ROUTING_LAYER            "M2"   ;# Min routing layer
+set MAX_ROUTING_LAYER            "M9"   ;# Max routing layer
+
+
 set_ignored_layers \
     -min_routing_layer  $MIN_ROUTING_LAYER \
     -max_routing_layer  $MAX_ROUTING_LAYER
     
+
+
+check_routability
+
+check_routability > /mnt/hgfs/Gp_CV32e40p/ASIC-Implementauion-of-CV32E40S-RISC-V-core-/6-Routing/check_routability.rpt
+
+set_lib_cell_purpose -include all  [get_lib_cells -of [get_cells *]]
+
+
+set_app_option -name route.common.global_min_layer_mode -value allow_pin_connection
+set_app_option -name route.common.global_max_layer_mode -value soft
+set_app_option -name time.si_enable_analysis -value true
+set_app_option -name time.enable_si_timing_windows -value true
+
+
+route_global
+route_track
+route_detail
+
 route_opt
 
 
