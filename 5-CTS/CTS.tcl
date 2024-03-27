9946a7bc81059355a31e25d91f482f7a77d9f278
kholoud0 <khloud1khaled@gmail.com>
2024-03-28 00:59:43 +0200

    Update CTS.tcl

--- 5-CTS/CTS.tcl
+++ 5-CTS/CTS.tcl
@@ -42,16 +42,61 @@ set_lib_cell_purpose -include cts */*_INV_S_6*
 set_lib_cell_purpose -include cts */*_INV_S_8*
 check_design -checks pre_clock_tree_stage
 
-set_wire_track_pattern -site_def unit -layer M1  -mode uniform -mask_constraint {mask_two mask_one} -coord 0.037 -space 0.0074 -direction horizontal
+#set_wire_track_pattern -site_def unit -layer M1  -mode uniform -mask_constraint {mask_two mask_one} -coord 0.037 -space 0.0074 -direction horizontal
 
-set_wire_track_pattern -site_def unit -layer M2  -mode uniform -mask_constraint {mask_two mask_one} -coord 0.0037 -space 0.00074 -direction vertical
+#set_wire_track_pattern -site_def unit -layer M2  -mode uniform -mask_constraint {mask_two mask_one} -coord 0.0037 -space 0.00074 -direction vertical
 
 set_lib_cell_purpose -exclude cts */*_INV_S_20*
+
+
+set_app_options \-name cts.common.user_instance_name_prefix -value "CTS_"
+
+set_placement_spacing_label -name a \
+            -side both -lib_cells */*_INV_S_8*
+
+
+set_placement_spacing_label -name a \
+            -side both -lib_cells */*_INV_S_6*
+
+
+set_placement_spacing_label -name a \
+            -side both -lib_cells */*_INV_S_4*
+
+
+
+set_placement_spacing_label -name a \
+            -side both -lib_cells */*_INV_S_3*
+
+
+
+set_placement_spacing_label -name a \
+            -side both -lib_cells */*_INV_S_2*
+
+
+
+set_placement_spacing_rule -labels {a a} {0 3}
+
+
+set_placement_spacing_label -name b \
+            -side both -lib_cells */*_FDPRB_V3_2*
+
+set_placement_spacing_label -name x  -side both -lib_cells [get_lib_cells -of [get_cells *]]
+
+set_placement_spacing_rule -labels {a b} {0 10}
+
+set_placement_spacing_rule -labels {b b} {0 10}
+
+set_placement_spacing_rule -labels {x b} {0 10}
+
+#saed14rvt_ss0p6vm40c:SAEDRVT14_FDPRB_V3_2.frame
+
 
 create_routing_rule ROUTE_RULES -multiplier_spacing 0.0001 -multiplier_width 0.1
 set_clock_routing_rules -rules ROUTE_RULES -min_routing_layer M2  -max_routing_layer M6
 set_clock_tree_options -target_latency 0.000 -target_skew 0.000 
 set cts_enable_drc_fixing_on_data true
+
+
 
 
 clock_opt
